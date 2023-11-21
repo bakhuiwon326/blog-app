2e44516c42b4fc96062c1d15a5eb8c4ab864f878
unknown <powerup326@naver.com>
2023-11-22 01:12:00 +0900

    set project

---
+++ blog-app-api/src/test/java/heewon/blogappapi/BlogAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package heewon.blogappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
